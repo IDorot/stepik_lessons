608f3e135cd0334c4787b809cbf3da4eb88702fd
IDorot <iackya@yandex.ru>
2021-06-09 09:22:05 +0300

    module_4

--- module_4/part_1/test_items.py
+++ module_4/part_1/test_items.py
@@ -9,14 +9,16 @@ correct_button_text = {
     'es': 'Añadir al carrito',
     'fr': 'Ajouter au panier',
 }
+buy_button_class = "btn.btn-lg.btn-primary.btn-add-to-basket"
+page_lang_class = "no-js"
 
 def test_ui_language(browser, language):
     #Arrange
     browser.get(link)
 
     #Act
-    buy_button = browser.find_element_by_class_name("btn.btn-lg.btn-primary.btn-add-to-basket")
-    page_lang = browser.find_element_by_class_name("no-js").get_attribute("lang")
+    buy_button = browser.find_element_by_class_name(buy_button_class)
+    page_lang = browser.find_element_by_class_name(page_lang_class).get_attribute("lang")
 
     #Assert
     #lower() используется т.к. в en-GB версии страницы аттрибут lang равен "en-gb", из-за чего тест может упасть
