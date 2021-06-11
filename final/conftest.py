10e60c9cd42680bb26b6aa13998cd91f42054ae6
IDorot <iackya@yandex.ru>
2021-06-11 16:38:12 +0300

    module_4

---
+++ final/conftest.py
@@ -0,0 +1,26 @@
+import pytest
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+
+
+def pytest_addoption(parser):
+    parser.addoption(
+        "--language", action="store", default=None, help="Choose your language: ru, en-GB, es, fr"
+    )
+
+@pytest.fixture(scope="session")
+def browser(language):
+    options = Options()
+    options.add_experimental_option('prefs', {'intl.accept_languages': language})
+    browser = webdriver.Chrome(options=options)
+    print(f"\nstart browser for test in {language} ..")
+    yield browser
+    print("\nquit browser...")
+    browser.quit()
+
+
+
+
+@pytest.fixture(scope="session")
+def language(request):
+    return  request.config.getoption("language")
