43261bd00df700d317f4fd23710323e389a768dc
IDorot <iackya@yandex.ru>
2021-06-28 15:15:49 +0300

    Edited test cases

--- final/conftest.py
+++ final/conftest.py
@@ -25,3 +25,15 @@ def browser(language):
 @pytest.fixture(scope="session")
 def language(request):
     return  request.config.getoption("language")
+
+@pytest.fixture(scope="function")
+def browser_get_title_value(language):
+    options = Options()
+    options.add_experimental_option('prefs', {'intl.accept_languages': language})
+    browser = webdriver.Chrome(options=options)
+    print(f"\nstart browser for test in {language} ..")
+
+    yield browser
+    print("\nquit browser...")
+    browser.save_screenshot("1.png")
+    browser.quit()
