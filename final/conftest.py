bd28b0972edba753080fe18a08184e244c07ac77
IDorot <iackya@yandex.ru>
2021-06-30 19:38:32 +0300

    I FIXED IT! :O

--- final/conftest.py
+++ final/conftest.py
@@ -7,21 +7,24 @@ def pytest_addoption(parser):
     parser.addoption(
         "--language", action="store", default="en-GB", help="Choose your language: ru, en-GB, es, fr"
     )
-    parser.addoption('--browser_name', action='store', default="chrome",
+    parser.addoption("--browser_name", action="store", default="chrome",
                      help="Choose browser: chrome or firefox")
 
 
 @pytest.fixture(scope="function")
-def browser(language, request):
-    options = Options()
-    options.add_experimental_option('prefs', {'intl.accept_languages': language})
+def browser(request, language):
+    language = request.config.getoption("language")
     browser_name = request.config.getoption("browser_name")
     if browser_name == "chrome":
         print(f"\nstart chrome browser in {language} for test..")
-        browser = webdriver.Chrome()
+        options = Options()
+        options.add_experimental_option('prefs', {'intl.accept_languages': language})
+        browser = webdriver.Chrome(options=options)
     elif browser_name == "firefox":
         print(f"\nstart firefox browser in {language} for test..")
-        browser = webdriver.Firefox()
+        options = Options()
+        options.add_experimental_option('prefs', {'intl.accept_languages': language})
+        browser = webdriver.Firefox(options=options)
     else:
         print("Browser {} still is not implemented".format(browser_name))
     yield browser
